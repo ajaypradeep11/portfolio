1b9ccb8b40e0032c7a73b478ba55932aed3e1d15
Pradeep <mpradeep11@gmail.com>
2026-04-26 00:38:05 -0400

    Add (wedding) route group with serif theme layout

---
+++ src/app/(wedding)/layout.tsx
@@ -0,0 +1,27 @@
+import { Cormorant_Garamond } from "next/font/google";
+
+import styles from "./layout.module.scss";
+
+const serif = Cormorant_Garamond({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+  variable: "--font-wedding-serif",
+});
+
+export const metadata = {
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
+
+export default function WeddingLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className={`${serif.variable} ${styles.weddingRoot}`}>{children}</div>
+  );
+}
